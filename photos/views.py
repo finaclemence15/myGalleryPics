18e184a7011780d70cd2f0bbff6e79e5fe15b8fe
finaclemence15 <finaclemence15@gmail.com>
2019-10-10 15:18:36 +0200

    creation of home .html file

--- photos/views.py
+++ photos/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from . models import Images
 
 # Create your views here.
 
 # Create your views here.
 def welcome(request):
+    photos = Images.objects.all()
     # return HttpResponse('Welcome My Gallery.')
-    return render(request, 'welcome.html')
+    return render(request, 'all-news/home.html',{'photos':photos})
