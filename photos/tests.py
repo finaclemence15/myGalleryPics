5c828d6d7ca6bc1f84d8e1390cfe056514a678f9
finaclemence15 <finaclemence15@gmail.com>
2019-10-11 10:38:04 +0200

    testing update  method of Category model

--- photos/tests.py
+++ photos/tests.py
@@ -8,18 +8,18 @@ class ImagesTestClass(TestCase):
     def setUp(self):
         self.book= Images(image = 'book.jpg', name ='book', description ='about book')
         self.book1= Images(image = 'book1.jpg', name ='book1', description ='about book1')
-        # self.book.save_images()
 
         # Testing  instance
     def test_instance(self):
         self.assertTrue(isinstance(self.book,Images))
         
-        # Testing Save Method
+        # Testing Save Method of Image model
     def test_save_method(self):
         self.book.save_image()
         images = Images.objects.all()
         self.assertTrue(len(images) > 0)
         
+    # Testing  delete method of Image model    
     def test_delete(self):
         self.book.save_image()
         self.book1.save_image()
@@ -28,6 +28,7 @@ class ImagesTestClass(TestCase):
         images = Images.objects.all()
         self.assertTrue(len(images) == 1)
 
+    # Testing  update method of Image model
     def test_update(self):
         self.book.save_image()
         image = Images.objects.filter(image='book.jpg').first()
@@ -37,25 +38,29 @@ class ImagesTestClass(TestCase):
     
 class LocationTestClass(TestCase):
     
+    # set Up method
     def setUp(self):
         self.kigali= Location(location = 'Rwanda')
     
     # Testing  instance
     def test_instance(self):
         self.assertTrue(isinstance(self.kigali,Location))
-        
+    
+    # Testing  save method of Location model    
     def test_save_method(self):
         self.kigali.save_location()
         locations = Location.objects.all()
         self.assertTrue(len(locations) > 0)
-        
+    
+    # Testing  delete method of Location model    
     def test_delete(self):
         self.kigali.save_location()
         location = Location.objects.filter(location = 'Rwanda').first()
         delete = Location.objects.filter(id = location.id).delete()
         locations = Location.objects.all()
         self.assertTrue(len(locations) == 0)
-        
+    
+    # Testing  update method of Location model    
     def test_update(self):
         self.kigali.save_location()
         location = Location.objects.filter(location = 'Rwanda').first()
@@ -77,13 +82,21 @@ class CategoryTestClass(TestCase):
         categories = Category.objects.all()
         self.assertTrue(len(categories) > 0)
         
-    # Testing  delete method of Category model
-    
+    # Testing  delete method of Category model  
     def test_delete(self):
         self.travel.save_category()
         category = Category.objects.filter(category = 'food').first()
         delete = Category.objects.filter(id = category.id).delete()
         categories = Category.objects.all()
-        self.assertTrue(len(categories) == 0)            
-                 
+        self.assertTrue(len(categories) == 0)  
+        
+                  
+    # Testing  Update method of Category model
+    
+    def test_update(self):
+        self.travel.save_category()
+        category = Category.objects.filter(category = 'food').first()
+        update = Category.objects.filter(id = category.id).update(category = 'cars')
+        updated = Category.objects.filter(category = 'cars').first()
+        self.assertNotEqual(category.category, updated.category)             
                            
